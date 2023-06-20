726ac738f8acf9fec8e5f9072e26457492bcc98c
Richard Casanova <richardcasanova@Richards-MacBook-Pro.local>
2023-06-20 19:50:33 -0400

    next commit

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -7,4 +7,23 @@ export const getTodosGroupedByColumn = async () => {
     )
     const todos =  data.documents
     
+    const columns = todos.reduce((acc, todo) => {
+        if (!acc.get(todo.status)) {
+            acc.set(todo.status, {
+                id: todo.status, 
+                todos: [],
+            });
+        }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id, 
+            $createdAt: todo.$createdAt, 
+            title: todo.title, 
+            status: todo.status, 
+            ...(todo.image && { image: JSON.parse(todo.image)})
+        })
+        return acc
+    }, new Map<TypedColumn, Column>)  
+    console.log(columns)
 }
+
