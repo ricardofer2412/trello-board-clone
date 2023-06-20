61d1a6deacb6ce5fcef8fa9d925eda53e6fc8945
Richard Casanova <richardcasanova@Richards-MacBook-Pro.local>
2023-06-20 19:32:27 -0400

    first commit

---
+++ components/Board.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useBoardStore } from '@/store/BoardStore';
+import { useEffect } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
+function Board() {
+    const getBoard =  useBoardStore((state) => state.getBoard)
+    useEffect(() => {
+        console.log('test')
+        getBoard()
+    }, [getBoard]);
+
+  return (
+    <h1>Hello</h1>
+
+    // <DragDropContext>
+    //     <Droppable droppableId='board' direction='horizontal' type='column'>
+    //         {(provided) => (
+    //             <div>
+
+    //             </div>
+    //         )}
+    //     </Droppable>
+    // </DragDropContext>
+  )
+}
+
+export default Board
