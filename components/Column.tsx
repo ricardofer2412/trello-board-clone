25534616529802cb97d3d5285d7b7595682846ff
Richard Casanova <richardcasanova@Richards-MacBook-Pro.local>
2023-06-23 16:31:21 -0400

    add draggable

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,3 +1,4 @@
+import { PlusCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 import { Draggable, Droppable } from 'react-beautiful-dnd'
 
@@ -7,6 +8,13 @@ type Props = {
     index: number;
 } 
 
+const idToColumnText: {
+   [key in TypedColumn]: string;
+} = {
+  todo: 'To Do', 
+  inprogress: 'In Progress', 
+  done: 'Done'
+}
 function Column ({id, todos, index} : Props) {
   return (
     <div>
@@ -29,8 +37,41 @@ function Column ({id, todos, index} : Props) {
                       
                         }`}
                       >
-                        <h2>{id}</h2>
+                        <h2 className='flex justify-between font-bold text-xl'>{idToColumnText[id]}
+                        <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-2 text-sm font-normal">{todos.length}</span>
+                         </h2>
+                        
+                        <div className='space-y-2'>
+                          {todos.map((todo, index) => (
+                            <Draggable
+                              key={todo.$id}
+                              draggableId={todo.$id}
+                              index={index}
+                            >
+                              {(provided) => (
+                                <TodoCard
+                                  todo={todo}
+                                  index={index}
+                                  id={id}
+                                  innerRef={provided.innerRef}
+                                  draggableProps={provided.draggableProps}
+                                  dragHandleProps={provided.dragHandleProps}  
+                                  />
+                              )}
+                            </Draggable>
 
+                          ))}
+
+                          {provided.placeholder}
+                          <div className='flex items-end justify-end p-2'> 
+                            <button className='text-green-500 hover:text-green-600'>
+                              <PlusCircleIcon
+                                className='h-10 w-10'
+                                />
+                            </button>
+                          </div>
+                        </div>
+                          
                       </div>
                     )}
                   </Droppable>
